9c9f94bb87d3ec7d0c65bc078916749c137d90ea
Raul Miguel Alves Soares <raul.2021104876@unicap.br>
2022-11-16 11:20:48 +0000

    Sintatico do slide

--- CompiladorL3/src/compiladorl3/Sintatico.java
+++ CompiladorL3/src/compiladorl3/Sintatico.java
@@ -1,5 +1,8 @@
 package compiladorl3;
 
+import javax.imageio.plugins.tiff.GeoTIFFTagSet;
+import javax.management.RuntimeErrorException;
+
 public class Sintatico {
 
     private Lexico lexico;
@@ -9,7 +12,7 @@ public class Sintatico {
         this.lexico = lexico;
     }
 
-    public void S() {
+    public void Iniciar() {
         this.token = this.lexico.nextToken();
         this.E();
         if (this.token.getTipo() == Token.TIPO_FIM_CODIGO) {
@@ -18,26 +21,34 @@ public class Sintatico {
     }
 
     public void E() {
-        this.T();
+        this.Tipos();
         this.El();
     }
 
     public void El() {
         if (this.token.getTipo() == Token.TIPO_OPERADOR_ARITMETICO) {
-            this.OP();
-            this.T();
+            this.Operadores();
+            this.Tipos();
             this.El();
         } else {
         }
     }
 
-    public void T() {
+    public void Tipos() {
         if (this.token.getTipo() == Token.TIPO_IDENTIFICADOR | this.token.getTipo() == Token.TIPO_INTEIRO
                 | this.token.getTipo() == Token.TIPO_REAL) {
                     
             this.token = this.lexico.nextToken();
         }else{
-            
+            throw new RuntimeException("Oxe, era para ser um identificador ou número pertinho de " + this.token.getLexema());
+        }
+    }
+
+    public void Operadores(){
+        if(this.token.getTipo() == Token.TIPO_OPERADOR_ARITMETICO){
+            this.token = this.lexico.nextToken();
+        }else{
+            throw new RuntimeException("Oxe, era para ter um operador aritmético (+,-,/,*) pertinho de " + this.token.getLexema());
         }
     }
 
